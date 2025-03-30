eff907f805d6d4e932a42d2f1b7afe02d697a4b0
thomas-henley <thomas.e.henley@gmail.com>
2025-03-29 22:33:31 -0400

    pass 1st stage

--- src/main.cs
+++ src/main.cs
@@ -2,7 +2,7 @@ using System.Net;
 using System.Net.Sockets;
 
 // Uncomment this line to pass the first stage
-// Console.Write("$ ");
+Console.Write("$ ");
 
 // Wait for user input
 Console.ReadLine();
