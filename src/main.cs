e20964fc270418624f549437a47fade7aacc3561
thomas-henley <thomas.e.henley@gmail.com>
2025-03-29 22:43:58 -0400

    repl

--- src/main.cs
+++ src/main.cs
@@ -1,15 +1,18 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
+while (true)
+{
+    Console.Write("$ ");
 
-// Wait for user input
-var input = Console.ReadLine() ?? string.Empty;
-InvalidCommand(input);
+    // Wait for user input
+    var input = Console.ReadLine() ?? string.Empty;
+    InvalidCommand(input);
+}
+
 return;
 
 void InvalidCommand(string command)
 {
-    Console.WriteLine($"{input}: command not found");
+    Console.WriteLine($"{command}: command not found");
 }
