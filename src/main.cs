e5e7b8414508afa210349d9cdcf757ded8ce9434
thomas-henley <thomas.e.henley@gmail.com>
2025-03-29 22:40:05 -0400

    handle invalid commands

--- src/main.cs
+++ src/main.cs
@@ -5,4 +5,11 @@ using System.Net.Sockets;
 Console.Write("$ ");
 
 // Wait for user input
-Console.ReadLine();
+var input = Console.ReadLine() ?? string.Empty;
+InvalidCommand(input);
+return;
+
+void InvalidCommand(string command)
+{
+    Console.WriteLine($"{input}: command not found");
+}
